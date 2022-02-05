5194a139590cb89344754cc708f46cfb59413de1
moseskarani <karanim594@gmail.com>
2022-02-05 20:45:08 +0300

    feat: first if statement

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ function jsQuiz(){
   var answer4 = document.questions.question4.value;
   var answer5 = document.questions.question5.value;
   var answer6 = document.questions.question6.value;
-  if (answer1 == "It is a scripting language used to create interactivity in web pages.")
+  if (answer1 == "It is a scripting language used to create interactivity in web pages.")(myScore++)
+  if (answer2 == "Alert")(myScore++)
+  if (answer3 == "False")(myScore++)
+  if (answer4 == "Client")(myScore++)
+  if (answer5 == "alert(“Javascript Quiz”);")(myScore++)
+  if (answer6 == "Default")(myScore++)
 }
