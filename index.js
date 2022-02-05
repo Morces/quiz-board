3eb806eade8c125d2efeadf18a0da909c02bf725
moseskarani <karanim594@gmail.com>
2022-02-05 20:36:12 +0300

    feat: create a function for answers and score

---
+++ index.js
@@ -0,0 +1,10 @@
+function jsQuiz(){
+  var myScore = 0;
+  var answer1 = document.questions.question1.value;
+  var answer2 = document.questions.question2.value;
+  var answer3 = document.questions.question3.value;
+  var answer4 = document.questions.question4.value;
+  var answer5 = document.questions.question5.value;
+  var answer6 = document.questions.question6.value;
+  if (answer1 == "It is a scripting language used to create interactivity in web pages.")
+}
