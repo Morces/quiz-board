7116807b597f72abe70e5f60129a837da621013d
moseskarani <karanim594@gmail.com>
2022-02-06 10:49:52 +0300

    feat: optimizing js

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-function jsQuiz(){
+//let qz = document.getElementById("qz");
+  //qz.addEventListener("submit". function() {
+  //  alert ("Form submitted successfully")
+  //})
+
+function score(){
+  var answer1 = document.questions.q1.value;
+  var answer2 = document.questions.q2.value;
+  var answer3 = document.questions.q3.value;
+  var answer4 = document.questions.q4.value;
+  var answer5 = document.questions.q5.value;
+  var answer6 = document.questions.q6.value;
+ 
+  var result  = document.getElementsById('result');
+  var qz = document.getElementsById("qz");
   var myScore = 0;
-  var answer1 = document.questions.question1.value;
-  var answer2 = document.questions.question2.value;
-  var answer3 = document.questions.question3.value;
-  var answer4 = document.questions.question4.value;
-  var answer5 = document.questions.question5.value;
-  var answer6 = document.questions.question6.value;
-  if (answer1 == "It is a scripting language used to create interactivity in web pages.")(myScore++)
-  if (answer2 == "Alert")(myScore++)
-  if (answer3 == "False")(myScore++)
-  if (answer4 == "Client")(myScore++)
-  if (answer5 == "alert(“Javascript Quiz”);")(myScore++)
-  if (answer6 == "Default")(myScore++)
-}
+  if (answer1 === "It is a scripting language used to create interactivity in web pages."){myScore +=10};
+  if (answer2 === "Alert"){myScore +=10};
+  if (answer3 === "False"){myScore +=10};
+  if (answer4 === "Client"){myScore +=10};
+  if (answer5 === "alert(“Javascript Quiz”);"){myScore +=10};
+  if (answer6 === "Default"){myScore +=10};
+qz.style.display = "none";
+result.textContent = myScore;
+  if (myScore==60) {
+    empty.textContent= "CONGRATULATIONS!! YOU SCORED " + myScore/6*100 + "%" + ". REMARKS: EXCELLENT."
+  }
+  else if (myScore==50) {
+    empty.textContent= "CONGRATULATIONS!! YOU SCORED " + myScore/6*100 + "%" + ". REMARKS: EXCELLENT."
+  };
+  else if (myScore==40) {
+    empty.textContent = "CONGRATULATIONS!! YOU SCORED " + myScore/6*100 + "%" + ". REMARKS: FAIR."
+  };
+  else if (myScore==30) {
+    empty.textContent = "CONGRATULATIONS!! YOU SCORED " + myScore/6*100 + "%" + ". REMARKS: FAIR."
+  };
+  else if (myScore<30) {
+    empty.textContent = "YOU HAVED PERFORMED POORLY. PLEASE RETAKE THE QUIZ"
+  }
+  }
+  
